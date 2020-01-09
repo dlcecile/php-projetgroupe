56d1563c6db75af5de08e7fa0d7835a2e7e9696c
antoineegret <a.egret@laposte.net>
2020-01-09 12:22:14 +0100

    ajout navbar

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <title>Maçonnerie Ocordo</title>
+    </head>
+    <body>
+        <div class="container-fluid p-0">
+            <header id="heroImage">
+                            <h1 class="text-center">Bienvenue chez Ocordo Maçonnerie</h1>
+            </header>
+            <nav class="navbar navbar-expand-lg row" style="width: 100%">
+                <button class="navbar-toggler align-self-end bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link text-white" href="index.php">Evenements <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php">Actus</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php">Trucs & Astuces</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    </span>
+</nav>
+<div class="bg-dark">
+    <p class="text-light">
+    </p>
+</div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
