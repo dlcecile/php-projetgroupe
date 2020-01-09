65a3099dd82478ca8cb037dab4785f7fa8daf3c6
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2020-01-09 21:59:52 +0100

    Merge fait, code corrigé et verifié. Site web à 90% finit

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="container-fluid m-0 p-0 text-white">
-    <div class="row">
+    <div class="row px-3">
         <div class="col-md-4">
             <h3 class="text-center mb-4">A propos</h3>
             <p class="footer text-justify">Entreprises de travaux de gros oeuvre et travaux de second oeuvre, Ocordo Travaux Amiens est une agence de travaux spécialisée dans l’externalisation du service commercial d’entreprises locales du bâtiment d'Amiens spécialisées dans les travaux de rénovation et de construction d’extensions. Nous avons l’expérience de plus de 3.000 projets de rénovations de maisons, rénovations d’appartements et de constructions d’extensions de maisons.</p>
@@ -14,7 +14,7 @@
         </div>
         <div class="col-md-4">
             <h3 class="text-center mb-4">Nous situer</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d41131.913301037785!2d2.300727!3d49.884823000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd3bd8e55d917e353!2sAgence%20de%20travaux%20Ocordo%20Amiens!5e0!3m2!1sfr!2sfr!4v1578568224492!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" allowfullscreen=""></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d41131.913301037785!2d2.300727!3d49.884823000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd3bd8e55d917e353!2sAgence%20de%20travaux%20Ocordo%20Amiens!5e0!3m2!1sfr!2sfr!4v1578568224492!5m2!1sfr!2sfr" frameborder="0" allowfullscreen=""></iframe>
         </div>
     </div>
 </footer>
--- index.php
+++ index.php
@@ -1,48 +1,49 @@
 <?php
 // Récupération du fichier XML
 $xml = simplexml_load_string(file_get_contents("source.xml"));
-//si mon get contient un id
+// Si mon get contient un id
 if (isset ($_GET['id'])){
-    // alors tu me récupère le chiffre de cette id
-    $id = (int)$_GET['id'];
-    //sinon
+  // Alors tu me récupère le chiffre de cette id
+  $id = (int)$_GET['id'];
+  // Sinon
 } else {
-    // l'id est égale à 0 ce qui correspond à mon accueil de mon id
-    $id = 0;
+  // l'id est égale à 0 ce qui correspond à mon accueil de mon id
+  $id = 0;
 } ?>
 <!doctype html>
 <html lang="fr" dir="ltr">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/2.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <title>Maçonnerie Ocordo</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/footer.css">
+  <link rel="stylesheet" href="assets/css/<?= ($id + 1) ?>.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <title>Maçonnerie Ocordo</title>
 </head>
 <body>
-<div class="container-fluid p-0">
+  <div class="container-fluid p-0">
     <header id="heroImage">
-        <h1 class="text-center">Bienvenue chez Ocordo Maçonnerie</h1>
+      <h1 class="text-center">Bienvenue chez Ocordo Maçonnerie</h1>
     </header>
-    <nav class="navbar navbar-expand-lg row" style="width: 100%">
-        <button class="navbar-toggler align-self-end bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <?php
-                // parcours mon fichier xml et va me chercher la clé qui correspond à l'id
-                foreach($xml as $key) {?>
-                    <!-- pour chaque ID,tu rajoutes le .html, on recupère le menu de l'id dont la clé qui correspond au menu pour faire la barre de navigation -->
-                    <li class="nav-item active"><a class="nav-link text-white" href ="<?= $key['id'] . '.html' ?>"><?= $key->menu ?></a></li>
-                <?php } ?>
-            </ul>
-        </div>
-    </nav>
-</div>
+    <nav class="navbar navbar-expand-lg row m-0" style="width: 100%">
+      <button class="navbar-toggler align-self-end bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <?php
+          // parcours mon fichier xml et va me chercher la clé qui correspond à l'id
+          foreach($xml as $key) {?>
+            <!-- pour chaque ID,tu rajoutes le .html, on recupère le menu de l'id dont la clé qui correspond au menu pour faire la barre de navigation -->
+            <li class="nav-item active"><a class="nav-link text-white" href ="<?= $key['id'] . '.html' ?>"><?= $key->menu ?></a></li>
+          <?php } ?>
+        </ul>
+      </div>
+    </div>
+  </span>
+</nav>
 <?php
 // Affiche la page
 echo $xml->page[$id]->titre;
@@ -53,6 +54,5 @@ include 'footer.php'; ?>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<script src="assets/css/4.css"></script>
 </body>
 </html>
