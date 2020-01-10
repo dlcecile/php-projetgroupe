907646dc03c8930e935495cb4b93c7bab35bf1f5
dlcecile <deleucecile@outlook.fr>
2020-01-10 11:56:04 +0100

    verification et modification projet terminé

--- footer.php
+++ footer.php
@@ -1,18 +1,19 @@
 <footer class="container-fluid m-0 p-0 text-white">
     <div class="row px-3">
-        <div class="col-md-4">
+        <div class="col-lg-4">
             <h3 class="text-center mb-4">A propos</h3>
             <p class="footer text-justify">Entreprises de travaux de gros oeuvre et travaux de second oeuvre, Ocordo Travaux Amiens est une agence de travaux spécialisée dans l’externalisation du service commercial d’entreprises locales du bâtiment d'Amiens spécialisées dans les travaux de rénovation et de construction d’extensions. Nous avons l’expérience de plus de 3.000 projets de rénovations de maisons, rénovations d’appartements et de constructions d’extensions de maisons.</p>
         </div>
-        <div class="col-md-4 border border-light border-top-0 border-bottom-0">
+        <div class="col-lg-4 border border-light border-top-0 border-bottom-0">
             <h3 class="text-center mb-4">Nous contacter</h3>
             <p class="footer">Ocordo Travaux Amiens</p>
             <p class="footer">31 Rue Alexandre</p>
             <p class="footer">80000 Amiens</p>
             <p class="footer">Tel : +33 (0)3 22 72 22 22</p>
             <p class="footer">Email : contact@ocordo-travaux.fr</p>
+              <img src="assets/img/logo.png" alt="logoentreprise" id="logo" class="responsive">
         </div>
-        <div class="col-md-4">
+        <div class="col-lg-4">
             <h3 class="text-center mb-4">Nous situer</h3>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d41131.913301037785!2d2.300727!3d49.884823000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd3bd8e55d917e353!2sAgence%20de%20travaux%20Ocordo%20Amiens!5e0!3m2!1sfr!2sfr!4v1578568224492!5m2!1sfr!2sfr" frameborder="0" allowfullscreen=""></iframe>
         </div>
